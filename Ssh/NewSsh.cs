fe89d32a84b461e5eecf87f9c1f6411ea2827a94
Silverlink34 <brandongaliher7@gmail.com>
2015-04-27 09:10:56 -0400

    commiting to work on other pc

--- Ssh/NewSsh.cs
+++ Ssh/NewSsh.cs
@@ -9,20 +9,43 @@ namespace NewSsh
 	[Cmdlet(VerbsCommon.New, "Ssh")]
 	public class NewSsh : PSCmdlet
 	{
-        [Parameter(Position = 0)]
-        
-        public string Server
+        //Declare the first parameter (by position, not a flag) and specify help message.
+        [Parameter(
+            Position = 0,
+            HelpMessage = "Server to connect to via ssh."
+        )]
+        //The public string here is the actual parameter
+        public String Server
         {
-            get { return server; }
-            set { server = value; }
+        get {return server;}
+        set {server = value;}
         }
         private string server;
-		protected override void ProcessRecord()
+        protected override void ProcessRecord()
 		{
-            if (Ser)
-            WriteObject("Please enter your username.");
-            String username = this.InvokeCommand.InvokeScript("Read-Host").ToString();
-            //SshClient sshClient = new SshClient(server,)
+            if (server.Equals(""))
+            {
+                serverPrompt();
+            }
+            else 
+            {
+                WriteObject("The specified server is: " + server);
+                usernamePrompt();
+            }
 		}
+        private String usernamePrompt()
+        {
+            WriteObject("Please enter your username.");
+            var input = this.InvokeCommand.InvokeScript("Read-Host");
+            String username = input.First().BaseObject.ToString();
+            return username;
+        }
+        private String serverPrompt()
+        {
+            WriteObject("Please enter the server name or ip address.");
+            var input = this.InvokeCommand.InvokeScript("Read-Host");
+            String server = input.First().BaseObject.ToString();
+            return server;
+        }
 	}
 }
